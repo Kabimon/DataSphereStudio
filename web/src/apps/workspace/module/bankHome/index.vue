a1308fd02a6e555ea01a9989b8cbc2d1340cec43
liangxm-swan <1450847653@qq.com>
2021-11-11 17:26:10 +0800

    UI样式修改

--- web/src/apps/workspace/module/bankHome/index.vue
+++ web/src/apps/workspace/module/bankHome/index.vue
@@ -3,11 +3,11 @@
     <div class="page-bgc-header">
       <div class="header-info">
         <div class="info-name">
-          <span>{{$t('message.workspace.workspaceTitle')}}</span>
+          <span>{{ $t("message.workspace.workspaceTitle") }}</span>
           <span class="workspace-name">{{ workspaceData.name }}</span>
         </div>
         <!-- <h1>{{$t('message.workspace.workspaceHeader', {name: workspaceData.name})}}</h1> -->
-        <p>{{$t('message.workspace.workspaceHeaderDesc')}}</p>
+        <p>{{ $t("message.workspace.workspaceHeaderDesc") }}</p>
       </div>
     </div>
     <div class="workspace-main">
@@ -15,36 +15,72 @@
         <div class="app-list-main">
           <div class="app-list-tabs">
             <Tabs v-model="showTab">
-              <Tab-pane name="dev" :label="$t('message.workspace.DSSDEV')"></Tab-pane>
-              <Tab-pane name="shop" :label="$t('message.workspace.DSSSHOP')"></Tab-pane>
+              <Tab-pane
+                name="dev"
+                :label="$t('message.workspace.DSSDEV')"
+              ></Tab-pane>
+              <Tab-pane
+                name="shop"
+                :label="$t('message.workspace.DSSSHOP')"
+              ></Tab-pane>
             </Tabs>
             <div v-show="showTab === 'dev'">
               <porjectComponent
                 ref="project"
                 @open-workflow="openWorkflow"
               ></porjectComponent>
-              <appProcess></appProcess>
+              <!-- <appProcess></appProcess> -->
             </div>
             <div v-show="showTab === 'shop'">
               <div class="app-shop-top">
                 <h3 class="item-header">
-                  <span>{{$t('message.workspace.QuickAccess')}}</span>
+                  <span>{{ $t("message.workspace.QuickAccess") }}</span>
                   <!-- 设置按钮 -->
                   <div class="setting-bt-wrap" v-if="!setting">
-                    <Button type="text" size="large" icon="md-settings" @click="setting=!setting">{{$t('message.workspace.home.setting')}}</Button>
+                    <Button
+                      type="text"
+                      size="large"
+                      icon="md-settings"
+                      @click="setting = !setting"
+                      >{{ $t("message.workspace.home.setting") }}</Button
+                    >
                   </div>
                   <div class="setting-bt-wrap" v-else>
-                    <Button type="text" size="large" icon="md-settings" @click="setting=!setting">{{$t('message.workspace.home.exit')}}</Button>
+                    <Button
+                      type="text"
+                      size="large"
+                      icon="md-settings"
+                      @click="setting = !setting"
+                      >{{ $t("message.workspace.home.exit") }}</Button
+                    >
                   </div>
                 </h3>
                 <div class="app-list">
-                  <div class="nodata-tips" v-if="favoriteApps.length===0">{{$t('message.workspace.home.tips')}}</div>
-                  <div v-else v-for="(item, index) in favoriteApps" :key="item.title" class="app-item-wrap" :class="{shadow:setting}" @click="setting?null:linkTo(item, item.url)">
-                    <div v-if="setting" class="close-wrap" @click.stop="deleteFavoriteApp(item.id, index)">
+                  <div class="nodata-tips" v-if="favoriteApps.length === 0">
+                    {{ $t("message.workspace.home.tips") }}
+                  </div>
+                  <div
+                    v-else
+                    v-for="(item, index) in favoriteApps"
+                    :key="item.title"
+                    class="app-item-wrap"
+                    :class="{ shadow: setting }"
+                    @click="setting ? null : linkTo(item, item.url)"
+                  >
+                    <div
+                      v-if="setting"
+                      class="close-wrap"
+                      @click.stop="deleteFavoriteApp(item.id, index)"
+                    >
                       <Icon type="md-close" size="20"></Icon>
                     </div>
-                    <SvgIcon class="app-icon" :icon-class="item.icon ? item.icon : 'shujukaifa-logo'" />
-                    <span class="label">{{$t('message.workspace.home.enter', {text: item.title})}}</span>
+                    <SvgIcon
+                      class="app-icon"
+                      :icon-class="item.icon ? item.icon : 'shujukaifa-logo'"
+                    />
+                    <span class="label">{{
+                      $t("message.workspace.home.enter", { text: item.title })
+                    }}</span>
                   </div>
                   <div class="app-item-add" @click="show = true">
                     <Icon type="md-add" size="25"></Icon>
@@ -60,74 +96,94 @@
         </div>
       </div>
       <div v-show="showTab === 'dev'" class="workspace-main-right">
-        <div class="right-box" v-for="(item, i) in sideDataList" :key="item.key">
+        <div
+          class="right-box"
+          v-for="(item, i) in sideDataList"
+          :key="item.key"
+        >
           <div class="knowledge-header">
             <span class="title">{{ item.name }}</span>
           </div>
           <ul class="knowledge-list">
-            <li v-for="(subItem, index) in item.contents" :key="index" class="knowledge-item" @click="openBrowser(subItem)">
-              <SvgIcon class="side-icon" :icon-class="subItem.icon"/>
-              {{subItem.title}}
+            <li
+              v-for="(subItem, index) in item.contents"
+              :key="index"
+              class="knowledge-item"
+              @click="openBrowser(subItem)"
+            >
+              <SvgIcon class="side-icon" :icon-class="subItem.icon" />
+              {{ subItem.title }}
             </li>
           </ul>
           <div v-if="i !== sideDataList.length - 1" class="line"></div>
         </div>
       </div>
     </div>
-    <Modal
-      v-model="show"
-      :title="$t('message.workspace.home.dlgTitle')">
+    <Modal v-model="show" :title="$t('message.workspace.home.dlgTitle')">
       <Form
         ref="dynamicForm"
         :model="formDynamic"
         :label-width="100"
         :rules="rules"
       >
-        <FormItem :label="$t('message.workspace.home.selectType')"
-          prop="selectType">
+        <FormItem
+          :label="$t('message.workspace.home.selectType')"
+          prop="selectType"
+        >
           <Select v-model="formDynamic.selectType">
-            <Option v-for="item in types" :value="`${item.id}`" :key="`${item.id}`">{{ item.title }}</Option>
+            <Option
+              v-for="item in types"
+              :value="`${item.id}`"
+              :key="`${item.id}`"
+              >{{ item.title }}</Option
+            >
           </Select>
         </FormItem>
 
-        <FormItem :label="$t('message.workspace.home.selectApp')"
-          prop="selectApp">
+        <FormItem
+          :label="$t('message.workspace.home.selectApp')"
+          prop="selectApp"
+        >
           <Select v-model="formDynamic.selectApp">
-            <Option v-for="item in apps" :value="`${item.id}`" :key="`${item.id}`" :disabled="item.had||!item.active">{{ item.title }}</Option>
+            <Option
+              v-for="item in apps"
+              :value="`${item.id}`"
+              :key="`${item.id}`"
+              :disabled="item.had || !item.active"
+              >{{ item.title }}</Option
+            >
           </Select>
         </FormItem>
       </Form>
 
       <div slot="footer">
-        <Button
-          @click="show = false"
-        >{{$t('message.workspace.home.cancel')}}</Button>
-        <Button
-          type="primary"
-          @click="addFavoriteApp"
-        >{{$t('message.workspace.home.save')}}</Button>
+        <Button @click="show = false">{{
+          $t("message.workspace.home.cancel")
+        }}</Button>
+        <Button type="primary" @click="addFavoriteApp">{{
+          $t("message.workspace.home.save")
+        }}</Button>
       </div>
-
     </Modal>
-    <Spin
-      v-if="loading"
-      size="large"
-      fix/>
+    <Spin v-if="loading" size="large" fix />
   </div>
 </template>
 <script>
-import api from '@/common/service/api';
-import util from '@/common/util';
-import mixin from '@/common/service/mixin';
-import porjectComponent from '../newProject';
-import applist from '../appList';
-import appProcess from '../appProcess/';
-import { GetWorkspaceData, GetWorkspaceApplications } from '@/common/service/apiCommonMethod.js';
+import api from "@/common/service/api";
+import util from "@/common/util";
+import mixin from "@/common/service/mixin";
+import porjectComponent from "../newProject";
+import applist from "../appList";
+import appProcess from "../appProcess/";
+import {
+  GetWorkspaceData,
+  GetWorkspaceApplications,
+} from "@/common/service/apiCommonMethod.js";
 export default {
   components: {
     porjectComponent: porjectComponent.component,
     applist: applist.component,
-    appProcess: appProcess.component
+    appProcess: appProcess.component,
   },
   mixins: [mixin],
   data() {
@@ -136,7 +192,7 @@ export default {
       loading: false,
       workspaceData: {
         name: "",
-        description: ""
+        description: "",
       },
       video: {},
       videos: [],
@@ -156,13 +212,21 @@ export default {
       workspaceId: null,
       rules: {
         selectApp: [
-          { required: true, message: this.$t('message.workspace.home.selectApp'), trigger: 'change' },
+          {
+            required: true,
+            message: this.$t("message.workspace.home.selectApp"),
+            trigger: "change",
+          },
         ],
         selectType: [
-          { required: true, message: this.$t('message.workspace.home.selectType'), trigger: 'change' },
+          {
+            required: true,
+            message: this.$t("message.workspace.home.selectType"),
+            trigger: "change",
+          },
         ],
       },
-    }
+    };
   },
   created() {
     this.workspaceId = this.$route.query.workspaceId;
@@ -171,23 +235,32 @@ export default {
   },
   watch: {
     $route() {
-      this.workspaceId= this.$route.query.workspaceId; //获取传来的参数
+      this.workspaceId = this.$route.query.workspaceId; //获取传来的参数
       this.init();
-    }
+    },
   },
   computed: {
-    types(){
-      return this.applications.map(item=>({title: item.title, id: item.id}))
+    types() {
+      return this.applications.map((item) => ({
+        title: item.title,
+        id: item.id,
+      }));
     },
-    apps(){
-      if(this.formDynamic.selectType){
-        const result = this.applications.find(item=>item.id==this.formDynamic.selectType);
-        result.appInstances.map(item=>{
+    apps() {
+      if (this.formDynamic.selectType) {
+        const result = this.applications.find(
+          (item) => item.id == this.formDynamic.selectType
+        );
+        result.appInstances.map((item) => {
           item.had = false;
-          if(this.favoriteApps.find(fItem=>item.id===fItem.menuApplicationId)){
+          if (
+            this.favoriteApps.find(
+              (fItem) => item.id === fItem.menuApplicationId
+            )
+          ) {
             item.had = true;
           }
-        })
+        });
         return result.appInstances;
       }
       return [];
@@ -195,9 +268,7 @@ export default {
   },
   methods: {
     // 进入工作流
-    openWorkflow() {
-
-    },
+    openWorkflow() {},
     // 进入知识库
     gotoKnowledge() {
       util.windowOpen(`http://${window.location.host}/wiki/scriptis`);
@@ -206,99 +277,128 @@ export default {
     gotoFAQ() {
       util.windowOpen(`http://${window.location.host}/kn`);
     },
-    linkTo(item){
-      this.gotoCommonIframe(item.name, {workspaceId: this.workspaceId})
+    linkTo(item) {
+      this.gotoCommonIframe(item.name, { workspaceId: this.workspaceId });
     },
     moreUtil() {
-      this.$router.push({ path: '/workspace', query: {...this.$route.query}});
+      this.$router.push({
+        path: "/workspace",
+        query: { ...this.$route.query },
+      });
     },
-    init(){
+    init() {
       this.$router.app.$on("getChangeCookies", () => {
         // 因为会重复注册一次，所以要触发后清除事件
         this.$router.app.$off("getChangeCookies");
-        GetWorkspaceData(this.workspaceId).then(data=>{
+        GetWorkspaceData(this.workspaceId).then((data) => {
           this.workspaceData = data.workspace;
-        })
+        });
 
-        api.fetch(`${this.$API_PATH.WORKSPACE_PATH}workspaces/${this.workspaceId}/favorites`, 'get').then(data=>{
-          this.favoriteApps = data.favorites;
-        })
+        api
+          .fetch(
+            `${this.$API_PATH.WORKSPACE_PATH}workspaces/${this.workspaceId}/favorites`,
+            "get"
+          )
+          .then((data) => {
+            this.favoriteApps = data.favorites;
+          });
 
-        GetWorkspaceApplications(this.workspaceId).then(data=>{
+        GetWorkspaceApplications(this.workspaceId).then((data) => {
           this.applications = data.applications || [];
-        })
+        });
       });
     },
-    deleteFavoriteApp(favoriteId, index){
-      api.fetch(`${this.$API_PATH.WORKSPACE_PATH}workspaces/${this.workspaceId}/favorites/${favoriteId}`, {}, { method: 'delete', data: {}}).then(()=>{
-        this.favoriteApps.splice(index, 1);
-      })
+    deleteFavoriteApp(favoriteId, index) {
+      api
+        .fetch(
+          `${this.$API_PATH.WORKSPACE_PATH}workspaces/${this.workspaceId}/favorites/${favoriteId}`,
+          {},
+          { method: "delete", data: {} }
+        )
+        .then(() => {
+          this.favoriteApps.splice(index, 1);
+        });
     },
-    addFavoriteApp(){
+    addFavoriteApp() {
       this.$refs.dynamicForm.validate((valid) => {
         if (valid) {
           // this.addAppLoading = true;
           const menuApplicationId = parseInt(this.formDynamic.selectApp, 10);
           const favoriteApp = this.findFavoriteAppsByMenuId(menuApplicationId);
-          if(favoriteApp) {
-            return this.$Message.error(`${this.$t('message.workspace.home.repeat')}`);
+          if (favoriteApp) {
+            return this.$Message.error(
+              `${this.$t("message.workspace.home.repeat")}`
+            );
           }
           this.show = false;
-          api.fetch(`${this.$API_PATH.WORKSPACE_PATH}workspaces/${this.workspaceId}/favorites`, {menuApplicationId: menuApplicationId},'post').then(data=>{
-            const app = this.findAppByApplicationId(this.formDynamic.selectApp)
-            this.favoriteApps.push({
-              ...app,
-              "id": data.favoriteId,
-              "menuApplicationId": app.id, //application表里的id
+          api
+            .fetch(
+              `${this.$API_PATH.WORKSPACE_PATH}workspaces/${this.workspaceId}/favorites`,
+              { menuApplicationId: menuApplicationId },
+              "post"
+            )
+            .then((data) => {
+              const app = this.findAppByApplicationId(
+                this.formDynamic.selectApp
+              );
+              this.favoriteApps.push({
+                ...app,
+                id: data.favoriteId,
+                menuApplicationId: app.id, //application表里的id
+              });
             });
-
-          })
         }
       });
     },
-    findAppByApplicationId(id){
+    findAppByApplicationId(id) {
       const arr = this.flatApps();
-      const result = arr.find(item=>item.id==id);
+      const result = arr.find((item) => item.id == id);
       return result;
     },
 
-    findFavoriteAppsByMenuId(menuId){
+    findFavoriteAppsByMenuId(menuId) {
       const arr = this.favoriteApps;
-      const result = arr.find(item=>item.menuApplicationId==menuId);
+      const result = arr.find((item) => item.menuApplicationId == menuId);
       return result;
     },
 
-    flatApps(){
+    flatApps() {
       const arr = [];
-      this.applications.forEach(item=>{
-        arr.push(...item.appInstances)
-      })
+      this.applications.forEach((item) => {
+        arr.push(...item.appInstances);
+      });
       return arr;
     },
-    iconSplit(icon){
-      if(icon){
-        return icon.split('|')
+    iconSplit(icon) {
+      if (icon) {
+        return icon.split("|");
       }
-      return ['','']
+      return ["", ""];
     },
     // 打开新页面查看问题
     openBrowser(item) {
       // 有可能是自己系统相对路径和，其他系统绝对路径
       if (item.urlType === 0) {
-        this.$router.push({path: item.url, query: this.$route.query})
+        this.$router.push({ path: item.url, query: this.$route.query });
       } else {
         util.windowOpen(item.url);
       }
     },
     // 获取常见问题
     getSideDatas() {
-      api.fetch(`${this.$API_PATH.WORKSPACE_PATH}getSideInfos`, {
-        workspaceID: 106
-      }, 'get').then((res) => {
-        this.sideDataList = res.presentations
-      })
-    }
-  }
-}
+      api
+        .fetch(
+          `${this.$API_PATH.WORKSPACE_PATH}getSideInfos`,
+          {
+            workspaceID: 106,
+          },
+          "get"
+        )
+        .then((res) => {
+          this.sideDataList = res.presentations;
+        });
+    },
+  },
+};
 </script>
 <style lang="scss" scoped src="./index.scss"></style>
--- web/src/dss/module/header/navMenu/index.vue
+++ web/src/dss/module/header/navMenu/index.vue
@@ -1,66 +1,88 @@
 <template>
   <div class="nav-menu">
-    <div class="nav-menu-left">
+    <div class="nav-menu-item" v-for="item in menuList" :key="item.id">
+      <div class="menu-item-title">
+        <div class="text">{{ item.title }}</div>
+      </div>
+      <div class="menu-item-content">
+        <div
+          @click.stop="handleClick(app)"
+          v-for="app in item.appInstances"
+          :key="app.id"
+          class="cont-item"
+        >
+          <span>{{ app.title }}</span>
+          <SvgIcon
+            icon-class="stars"
+            style="font-size: 20px"
+            color="#4f99ef"
+          ></SvgIcon>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="nav-menu-left">
       <template v-for="(item, index) in menuList">
         <div
           class="nav-menu-left-item"
           :key="index"
-          :class="{'actived':current && item.id === current.id}"
-          @mouseover.stop="onMouseOver(item)">
-          <!-- <p class="nav-menu-left-sub-title">{{item.subTitle}}</p> -->
-          <p class="nav-menu-left-main-title">{{item.title}}</p>
+          :class="{ actived: current && item.id === current.id }"
+          @mouseover.stop="onMouseOver(item)"
+        >
+          <p class="nav-menu-left-main-title">{{ item.title }}</p>
           <Icon
             size="14"
             type="ios-arrow-forward"
             class="nav-menu-left-icon"
-            v-if="item.appInstances"></Icon>
+            v-if="item.appInstances"
+          ></Icon>
         </div>
       </template>
     </div>
-    <div
-      class="nav-menu-right"
-      v-if="current">
-      <div
-        class="nav-menu-right-item"
-        :key="current.id">
-        <div class="nav-menu-right-item-title">{{current.title}}</div>
+    <div class="nav-menu-right" v-if="current">
+      <div class="nav-menu-right-item" :key="current.id">
+        <div class="nav-menu-right-item-title">{{ current.title }}</div>
         <div
           v-for="item in current.appInstances"
           class="nav-menu-right-item-child"
           :key="item.title"
-          @click.stop="handleClick(item)">
-          <SvgIcon class='nav-menu-right-item-icon' :icon-class="iconSplit(item.icon)[0]" :color="iconSplit(item.icon)[1]"/>
-          <span>{{item.title}}</span>
+          @click.stop="handleClick(item)"
+        >
+          <SvgIcon
+            class="nav-menu-right-item-icon"
+            :icon-class="iconSplit(item.icon)[0]"
+            :color="iconSplit(item.icon)[1]"
+          />
+          <span>{{ item.title }}</span>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 <script>
-import mixin from '@/common/service/mixin';
+import mixin from "@/common/service/mixin";
 export default {
   data() {
     return {
-      current: null
-    }
+      current: null,
+    };
   },
   props: {
     menuList: {
       type: Array,
-      default: () => []
-    }
+      default: () => [],
+    },
   },
-  mixins: [ mixin ],
+  mixins: [mixin],
   methods: {
-    iconSplit(icon){
-      if(icon){
-        return icon.split('|')
+    iconSplit(icon) {
+      if (icon) {
+        return icon.split("|");
       }
-      return ['','']
+      return ["", ""];
     },
     onMouseOver(item) {
       if (item.appInstances) {
-        return this.current = item;
+        return (this.current = item);
       }
       this.current = null;
     },
@@ -69,12 +91,12 @@ export default {
         let query = this.$route.query;
         this.gotoCommonIframe(child.name, query);
       } else {
-        this.$Message.warning(this.$t('message.common.warning.comingSoon'));
+        this.$Message.warning(this.$t("message.common.warning.comingSoon"));
       }
-      this.$emit('click', child);
-    }
+      this.$emit("click", child);
+    },
   },
-}
+};
 </script>
 <style lang="scss" src="./index.scss">
 </style>
